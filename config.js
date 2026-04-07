a6db04be10a8f085d794d420be4941dd644de8dc
S-Tomoharu <s.tomoharu@gmail.com>
2026-04-08 08:29:14 +0900

    Add config file for GAS integration

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxCaX9OZ4Ui86bBZEN-bheDvKfcYn_aAVNxyFKYGWl719WTVB9mSxtgDDeVK0KqwggRiQ/exec',
+  SUBMISSION_GAS_URL: 'https://script.google.com/macros/s/AKfycbyXJxUVoIBFfCEzVgv39sZRDSUwmAagP3VAm5s5x_G54xhnZBLsdb2U2Tx2ceWLih8B/exec',
+  ADMIN_URL: 'https://script.google.com/a/macros/tokai-jh.ed.jp/s/AKfycbx2-NMn6lrOpLA6ysdsbFT0lYd_OBcZKwGGO-8__128HDMa7yAUxm3IWAUSs6uFiiBEiw/exec',
+
+  SHEET: '2026年度',
+  YEAR: '2026',
+
+  YEAR_CONFIG: {"2026":[{"class":"2A","max":40},{"class":"2B","max":40},{"class":"2E","max":37},{"class":"2J","max":48}]}
+};
